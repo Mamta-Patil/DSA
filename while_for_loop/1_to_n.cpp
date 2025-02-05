0fd7c1d20dac0dcbdfa6446abf5cba9ba559389b
MamtaPatil <mamtapatil5252@gmail.com>
2025-02-05 16:17:09 +0530

    loop

---
+++ while_for_loop/1_to_n.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;int i=1;
+	cout << "Enter value of n :" << endl;
+	cin >> n;
+	
+	while(i<=n)
+	{
+	 cout << i <<endl;
+	 i++;
+	}
+	
+}
---
+++ while_for_loop/leep_year.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int first_year=2000;
+  while(first_year <= 4000)
+  {
+  	 cout << first_year << endl;  
+  	 first_year+=4;
+  }
+}
+
