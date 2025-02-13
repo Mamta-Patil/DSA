261cab344182cedf3c22f9d67d164cf5f243a86f
MamtaPatil <mamtapatil5252@gmail.com>
2025-02-13 08:04:36 +0530

    pattern

---
+++ SPACE_PATTERN/pattern3.cpp
@@ -0,0 +1,24 @@
+//         5
+//       4 5
+//     3 4 5
+//   2 3 4 5
+// 1 2 3 4 5
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5; 
+    
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < (n - i - 1); j++) {
+            cout << "  "; // Print spaces for alignment
+        }
+        for (int j = (n - i); j <= n; j++) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
+
